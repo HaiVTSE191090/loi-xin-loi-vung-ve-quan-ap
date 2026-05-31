34c4d8a8601d5d51fe6acec4f14ef9478dd8567a
Vũ Thanh Hải <haithanhvu0501@gmail.com>
2026-05-31 22:13:18 +0700

    Finalll

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,30 @@ const repoName = "loi-xin-loi-vung-ve-quan-ap";
 const isGithubPages = process.env.GITHUB_ACTIONS === "true";
 
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   trailingSlash: true,
+//   allowedDevOrigins: ["127.0.0.1", "localhost"],
+//   env: {
+//     NEXT_PUBLIC_BASE_PATH: isGithubPages ? `/${repoName}` : ""
+//   },
+//   images: {
+//     unoptimized: true
+//   },
+//   basePath: isGithubPages ? `/${repoName}` : "",
+//   assetPrefix: isGithubPages ? `/${repoName}/` : ""
+// };
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   allowedDevOrigins: ["127.0.0.1", "localhost"],
   env: {
-    NEXT_PUBLIC_BASE_PATH: isGithubPages ? `/${repoName}` : ""
+    NEXT_PUBLIC_BASE_PATH: "",
   },
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  basePath: isGithubPages ? `/${repoName}` : "",
-  assetPrefix: isGithubPages ? `/${repoName}/` : ""
 };
 
 export default nextConfig;
